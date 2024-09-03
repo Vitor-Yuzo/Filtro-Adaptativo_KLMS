de4ed881de8d1b4c6bb3f2c8aff952e606a585cc
VITOR YUZO TAKEI <vitor.yuzo.profissional@gmail.com>
2024-09-03 15:08:37 -0300

    codigo_principal_versao_03
    Implementando a atualização de dicionário

--- ALGORITMO KLMS - Copia.c
+++ ALGORITMO KLMS - Copia.c
@@ -36,6 +36,31 @@ void imprime_teste(int *vetor,int tamanho)
 
 // -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+double *atualizar_dicionario(double *Un, double *dicionario, int n, int M)
+{
+    for(int i=M-1; i>0; i--)
+    {
+        dicionario[i] = dicionario[i-1];
+    }
+
+    dicionario[0] = Un[n];
+
+    printf("DICIONARIO:\n");
+    imprime_teste_1(dicionario,M);
+    printf("\n");
+
+    printf("ENTRADA (UN):\n");
+    imprime_teste_1(Un,10);
+    printf("\n");
+
+    system("pause");
+
+
+
+
+    return dicionario;
+}
+
 double calculo_e2(double e2, double e)
 {
     e2 = e2 + (pow(e,2));
@@ -45,14 +70,18 @@ double calculo_e2(double e2, double e)
 
 
 //FUNÇÃO QUE CALCULA UM NOVO VALOR PARA O COEFICIENTE Wn
-double calculo_Wn(double Wn_n,double passo,double kw_n,double e)
+double *calculo_Wn(double *Wn,double passo,double *kw,double e,int M)
 {
     double aux, resultado;
 
-    aux = 2 * passo * kw_n * e;
-    resultado = Wn_n - aux;
+    for(int i=0; i<M; i++)
+    {
+        aux = 2 * passo * kw[i] * e;
+        resultado = Wn[i] - aux;
+        Wn[i] = resultado;
+    }
 
-    return resultado;
+    return Wn;
 }
 
 // FUNÇÃO QUE CALCULA O ESCALAR d
@@ -87,7 +116,7 @@ double calculo_dw(double *Wn, double *kw, int tamanho)
 
 
 // FUNÇÃO QUE CALCULA O kw(n) POSSÍVEL ERRO NESTA FUNÇÃO
-double calculo_kw(double *vetor_Un, double *vetor_Dn, int n)
+double *calculo_kw(double *kw, double *vetor_Un, double *vetor_Dn, int n,int M)
 {
     //Declarando Variáveis
     double aux;         // Variável que guarda a subtração de Un e Dn
@@ -100,22 +129,28 @@ double calculo_kw(double *vetor_Un, double *vetor_Dn, int n)
     //printf("Un: %lf\n",vetor_Un[n]);
     //printf("Dn: %1f\n",vetor_Dn[n]);
 
-    aux = (vetor_Un[n] - vetor_Dn[n]);
-    aux_1 = pow(0.3,2);
+    for(int i=0; i<M; i++)
+    {
+        aux = (vetor_Un[n] - vetor_Dn[i]);
+        aux_1 = pow(0.3,2);
 
-    numerador = pow(aux,2);
-    denominador = aux_1 * 2;
+        numerador = pow(aux,2);
+        denominador = aux_1 * 2;
 
-    resultado = (numerador / denominador);
+        resultado = (numerador / denominador);
 
-    kw_n = 1 / (exp(resultado));
+        kw_n = 1 / (exp(resultado));
 
-    //printf("Numerador: %lf\n",numerador);
-    //printf("Denominador: %lf\n",denominador);
-    //printf("Resultado: %lf\n",resultado);
-    //printf("Kw(n): %lf\n\n",kw_n);
+        kw[i] = kw_n;
 
-    return kw_n;
+        //printf("Numerador: %lf\n",numerador);
+        //printf("Denominador: %lf\n",denominador);
+        //printf("Resultado: %lf\n",resultado);
+        //printf("Kw(n): %lf\n\n",kw_n);
+
+    }
+
+    return kw;
 }
 
 // FUNÇÃO QUE GERA O VETOR INICIAL DE COEFICIENTES COM 0 NAS POSIÇÕES
@@ -232,44 +267,31 @@ int main ()
 
     for(int m=0; m<100; m++)
     {
-        Un = cria_vetor_entrada(Un,10,N);       // Criando o vetor de entrada com valores aleatórios
-        Yn = operacao_exponencial(Un, Yn, N);   // Realizando a operção Exponencial y(n)
-        Zn = cria_vetor_ruido(Zn,1,N);          // Criando o vetor com os dados de ruido 10^(-5)
-        dicionario = cria_vetor_entrada(dicionario,2,M); // Criando o Vetor Dicionário
-        Wn = cria_vetor_coeficiente(Wn,M);      // Criando Vetor de coeficientes inicializados com 0
+        Un = cria_vetor_entrada(Un,10,N);                   // Criando o vetor de entrada com valores aleatórios
+        Yn = operacao_exponencial(Un, Yn, N);               // Realizando a operção Exponencial y(n)
+        Zn = cria_vetor_ruido(Zn,1,N);                      // Criando o vetor com os dados de ruido 10^(-5)
+        dicionario = cria_vetor_entrada(dicionario,2,M);    // Criando o Vetor Dicionário
+        Wn = cria_vetor_coeficiente(Wn,M);                  // Criando Vetor de coeficientes inicializados com 0
 
         for(int n=0; n<N; n++)
         {
             kw = cria_vetor_coeficiente(kw,M);      // Criando o vetor Kernel inicialmente zerado
-
-            // Calculando cada elemento do vetor Kernel
-            for(int i=0; i<M; i++)
-            {
-                kw[i] = calculo_kw(Un,dicionario,n);
-            }
-
-            dw = calculo_dw(Wn, kw, M); // Calculando o valor desejado Dw através de um produto escalar entre vetores
-            d = calculo_d(Yn,Zn,n);     // Calculando o valor de d
-            e = dw - d;                 // Calculando o erro
-
-            // Atualizando os valores do vetor de coeficientes Wn
-            for(int i=0; i<M; i++)
-            {
-                Wn[i] = calculo_Wn(Wn[i],passo,kw[i],e);
-            }
-
-            // ATUALIZAR DICIONÁRIO
-
+            kw = calculo_kw(kw,Un,dicionario,n,M);  // Calculando todos os valores de Kernel através da interação dos valores de entrada e dicionário
+            dw = calculo_dw(Wn, kw, M);             // Calculando o valor desejado Dw através de um produto escalar entre vetores
+            d = calculo_d(Yn,Zn,n);                 // Calculando o valor de d
+            e = dw - d;                             // Calculando o erro
+            Wn = calculo_Wn(Wn,passo,kw,e,M);       // Atualizando os valores do vetor coeficiente
+            dicionario = atualizar_dicionario(Un,dicionario,n,M); // Atualizando os valores do dicionario com os valores de entrada
             free(kw);
         }
 
-            e2 = calculo_e2(e2,e);
+        e2 = calculo_e2(e2,e);
 
-            free(Un);
-            free(Yn);
-            free(Zn);
-            free(dicionario);
-            free(Wn);
+        free(Un);
+        free(Yn);
+        free(Zn);
+        free(dicionario);
+        free(Wn);
     }
 
 
